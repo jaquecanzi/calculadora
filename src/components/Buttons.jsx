eb23d1827ae4179ec947010fcc78d4f43fa6613c
Jaqueline Copello <jaque.canzi@gmail.com>
2022-12-02 17:34:08 +0000

    buttons finished

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import './Buttons.css'
 
-export default props => 
-<button className={`
-    button
-    ${props.operation ? 'operation' : ''}
-    ${props.double ? 'double' : ''}
-    ${props.triple ? 'triple' : ''}
-    `
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    
+    return (
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
 }
-    onClick = {e => props.click && props.click(e.target.label)}
-
->
-    {props.label}
-</button>
