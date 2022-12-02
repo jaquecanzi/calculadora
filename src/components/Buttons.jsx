30e360d84efec6eca69d886190aae84c7c0f0cf6
Jaqueline Copello <jaque.canzi@gmail.com>
2022-12-02 17:04:21 +0000

    Buttons in progress

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Buttons.css'
+
+export default props => 
+<button className={`
+    button
+    ${props.operation ? 'operation' : ''}
+    ${props.double ? 'double' : ''}
+    ${props.triple ? 'triple' : ''}
+    `
+}
+    onClick = {e => props.click && props.click(e.target.label)}
+
+>
+    {props.label}
+</button>
